3703b2c901d0ffb186aa3df8cdc70468f83368b1
Adam Gazdiev <a.gazdiev@gmail.com>
2023-11-18 19:16:55 +0100

    new coordinate processing system

--- script.js
+++ script.js
@@ -1,68 +1,79 @@
-function toRadians(degrees) {
-    return degrees * Math.PI / 180;
+const GH_API_KEY = '6a6dbedd-0046-44fb-8dd3-f6bc14b8f9e4'; // Замените на ваш реальный ключ API
+
+async function calculateRouteDistanceGraphHopper(lat1, lon1, lat2, lon2) {
+    const ghUrl = `https://graphhopper.com/api/1/route?point=${lat1},${lon1}&point=${lat2},${lon2}&vehicle=car&key=${GH_API_KEY}`;
+    const response = await fetch(ghUrl);
+    const data = await response.json();
+
+    if (data.paths && data.paths.length > 0) {
+        return data.paths[0].distance; // Расстояние в метрах
+    } else {
+        throw new Error('Не удалось рассчитать маршрут');
+    }
 }
 
-function calculateDistance(lat1, lon1, lat2, lon2) {
-    const R = 6371e3; // Straal van de planeet Aarde in meters
-    const φ1 = toRadians(lat1);
-    const φ2 = toRadians(lat2);
-    const Δφ = toRadians(lat2 - lat1);
-    const Δλ = toRadians(lon2 - lon1);
-
-    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-              Math.cos(φ1) * Math.cos(φ2) *
-              Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-    return R * c; // Afstand
-}
-
-function findNearestParkings(lat, lon, parkings) {
-    parkings.forEach(parking => {
+async function findNearestParkingsGraphHopper(userLat, userLon, parkings) {
+    const parkingDistances = await Promise.all(parkings.map(async (parking) => {
         const parkingLat = parking.geo_point_2d.lat;
         const parkingLon = parking.geo_point_2d.lon;
-        const distance = calculateDistance(lat, lon, parkingLat, parkingLon);
-        parking.distance = distance;
-    });
+        try {
+            const distance = await calculateRouteDistanceGraphHopper(userLat, userLon, parkingLat, parkingLon);
+            return { ...parking, distance };
+        } catch (error) {
+            console.error('Ошибка при расчете расстояния:', error);
+            return { ...parking, distance: null };
+        }
+    }));
 
-    parkings.sort((a, b) => a.distance - b.distance);
-    return parkings.slice(0, 5); // Возвращаем 5 ближайших парковок
+    parkingDistances.sort((a, b) => a.distance - b.distance);
+    return parkingDistances.filter(parking => parking.distance !== null).slice(0, 5);
 }
 
-
-// ...
-
-function displayParkings(parkings) {
+function displayParkings(parkings, userLat, userLon) {
     const parkingList = document.getElementById('parkingList');
-    parkingList.innerHTML = ''; // De lijst leegmaken
+    parkingList.innerHTML = ''; // Очистка списка парковок
+    document.getElementById('backButton').style.display = 'inline-block'; // Показ кнопки "назад"
 
-    document.getElementById('backButton').style.display = 'inline-block'; // De 'Terug' knop tonen
+    let displayedParkings = new Set(); // Для отслеживания уникальных парковок
 
     parkings.forEach(parking => {
-        const div = document.createElement('div');
-        const coords = `${parking.geo_point_2d.lat}, ${parking.geo_point_2d.lon}`;
-        const distanceKm = (parking.distance / 1000).toFixed(2); // Afstand in kilometers
-        const capacity = parking.capaciteit ? parking.capaciteit : 'Niet beschikbaar';
-        const parkingUrl = parking.url ? `<a href="${parking.url}" target="_blank">Website</a>` : '';
-        const address = `${parking.straatnaam}${parking.huisnr ? ' ' + parking.huisnr : ''}`; // Controleren op huisnummer
+        // Проверяем, не была ли парковка уже отображена
+        const uniqueId = parking.naam + parking.straatnaam;
+        if (displayedParkings.has(uniqueId)) return;
+        displayedParkings.add(uniqueId);
 
-        div.className = 'parking-item';
+        const div = document.createElement('div');
+        div.className = 'parking-item'; // Убедитесь, что класс соответствует вашему CSS
+        const websiteButton = parking.url ? `<a href="${parking.url}" target="_blank" class="btn btn-secondary">Website</a>` : '';
+        
         div.innerHTML = `
             <h2>${parking.naam}</h2>
-            <p>Adres: ${address}</p>
-            <p>Capaciteit: ${capacity}</p>
-            <p>Afstand: ${distanceKm} km</p>
-            <p>${parkingUrl}</p>
-            <button onclick="copyToClipboard('${coords}')">Coördinaten kopiëren</button>
-            <button onclick="openInGoogleMaps('${coords}')">Open in Google Maps</button>
+            <p>Adres: ${parking.straatnaam} ${parking.huisnr || ''}</p>
+            <p>Capaciteit: ${parking.capaciteit || 'Niet beschikbaar'}</p>
+            <p>Afstand: ${(parking.distance / 1000).toFixed(2)} km</p>
+            <div>${websiteButton}</div>
+            <button onclick="copyToClipboard('${parking.geo_point_2d.lat}, ${parking.geo_point_2d.lon}')" class="btn btn-primary">Coördinaten kopiëren</button>
+            <button onclick="openRouteInMaps(${userLat}, ${userLon}, ${parking.geo_point_2d.lat}, ${parking.geo_point_2d.lon})" class="btn btn-primary">Open in Google Maps</button>
         `;
         parkingList.appendChild(div);
     });
 }
 
-// De rest van de functies blijven ongewijzigd
 
-// ...
+function openRouteInMaps(userLat, userLon, parkingLat, parkingLon) {
+    // Убедитесь, что все параметры являются числами
+    if (typeof userLat !== "number" || typeof userLon !== "number" ||
+        typeof parkingLat !== "number" || typeof parkingLon !== "number") {
+        console.error("Неверные координаты для прокладывания маршрута.");
+        return;
+    }
+
+    // Создаем URL-адрес с использованием этих параметров
+    const origin = encodeURIComponent(`${userLat},${userLon}`);
+    const destination = encodeURIComponent(`${parkingLat},${parkingLon}`);
+    const url = `https://www.google.com/maps/dir/?api=1&origin=${origin}&destination=${destination}&travelmode=driving`;
+    window.open(url, '_blank');
+}
 
 
 function copyToClipboard(text) {
@@ -73,26 +84,19 @@ function copyToClipboard(text) {
     });
 }
 
-function openInGoogleMaps(coords) {
-    const url = `https://www.google.com/maps/search/?api=1&query=${coords}`;
-    window.open(url, '_blank');
-}
-
 document.getElementById('findParking').addEventListener('click', function() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
+        navigator.geolocation.getCurrentPosition(async function(position) {
             const userLat = position.coords.latitude;
             const userLon = position.coords.longitude;
-
-            fetch('https://data.stad.gent/api/v2/catalog/datasets/locaties-openbare-parkings-gent/exports/json')
-            .then(response => response.json())
-            .then(parkings => {
-                const nearestParkings = findNearestParkings(userLat, userLon, parkings);
-                displayParkings(nearestParkings);
-            })
-            .catch(error => {
+            try {
+                const response = await fetch('https://data.stad.gent/api/v2/catalog/datasets/locaties-openbare-parkings-gent/exports/json');
+                const parkings = await response.json();
+                const nearestParkings = await findNearestParkingsGraphHopper(userLat, userLon, parkings);
+                displayParkings(nearestParkings, userLat, userLon);
+            } catch (error) {
                 console.error('Fout bij het aanvragen van de API:', error);
-            });
+            }
         }, function(error) {
             console.error('Fout bij ophalen van geolocatie:', error);
         });
@@ -101,13 +105,13 @@ document.getElementById('findParking').addEventListener('click', function() {
     }
 });
 
-
 document.getElementById('backButton').addEventListener('click', function() {
-    this.style.display = 'none'; // Verberg de terugknop
-    document.getElementById('parkingList').innerHTML = ''; // De lijst met resultaten wissen
-    document.getElementById('addressInput').value = ''; 
+    this.style.display = 'none'; // Скрыть кнопку "назад"
+    document.getElementById('parkingList').innerHTML = ''; // Очистить список парковок
+    document.getElementById('addressInput').value = ''; // Очистить ввод адреса
 });
-// Functie voor het geocoderen van een adres
+
+// Добавим функцию для геокодирования адреса
 function geocodeAddress(address) {
     const nominatimUrl = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(address)}`;
 
@@ -115,14 +119,14 @@ function geocodeAddress(address) {
         .then(response => response.json())
         .then(data => {
             if (data.length > 0) {
-                return { lat: data[0].lat, lon: data[0].lon };
+                return { lat: parseFloat(data[0].lat), lon: parseFloat(data[0].lon) };
             } else {
                 throw new Error('Geen resultaten gevonden voor dit adres');
             }
         });
 }
 
-// Gebeurtenishandler voor de zoekknop op adres
+// Обработчик событий для кнопки поиска по адресу
 document.getElementById('findParkingByAddress').addEventListener('click', function() {
     const address = document.getElementById('addressInput').value;
     if (address) {
@@ -131,8 +135,10 @@ document.getElementById('findParkingByAddress').addEventListener('click', functi
                 fetch('https://data.stad.gent/api/v2/catalog/datasets/locaties-openbare-parkings-gent/exports/json')
                 .then(response => response.json())
                 .then(parkings => {
-                    const nearestParkings = findNearestParkings(coords.lat, coords.lon, parkings);
-                    displayParkings(nearestParkings);
+                    return findNearestParkingsGraphHopper(coords.lat, coords.lon, parkings);
+                })
+                .then(nearestParkings => {
+                    displayParkings(nearestParkings, coords.lat, coords.lon);
                 })
                 .catch(error => {
                     console.error('Fout bij het aanvragen van de API:', error);
@@ -145,4 +151,3 @@ document.getElementById('findParkingByAddress').addEventListener('click', functi
         alert('Voer een geldig adres in.');
     }
 });
-
